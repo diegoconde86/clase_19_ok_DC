019c6259297c7c8f621e28bbb164b37fed4385f8
diegoconde86 <diegconde86@gmail.com>
2022-07-05 02:27:18 -0300

    primer commit del entregable

---
+++ AppCoder/models.py
@@ -0,0 +1,33 @@
+from distutils.archive_util import make_zipfile
+from pickle import TRUE
+from django.db import models
+
+class Curso(models.Model):
+
+    nombre=models.CharField(max_length=50)
+    comision=models.IntegerField()
+
+class Estudiante(models.Model):
+    nombre= models.CharField(max_length=50)
+    apellido= models.CharField(max_length=50)
+    email= models.EmailField()
+    
+class Profesor(models.Model):
+    nombre= models.CharField(max_length=50)
+    apellido= models.CharField(max_length=50)
+    email= models.EmailField()
+    profesion= models.CharField(max_length=50)
+    
+class Entregable(models.Model):
+    nombre= models.CharField(max_length=50)
+    fecha_entrega= models.DateField()
+    entregado= models.BooleanField()
+
+class Familia(models.Model):
+    nombre_apellido= models.CharField(max_length=50)
+    dni= models.IntegerField()
+    fecha_actualizacion= models.DateField(auto_now_add=TRUE)
+    ocupacion=models.CharField(max_length=50)
+    
+
+# Create your models here.
---
+++ AppCoder/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+from django.template import Context,Template,loader
+from django.shortcuts import render
+from AppCoder.models import Curso, Familia
+
+ 
+def curso(self):
+    curso= Curso(nombre="Django", comision=1234)
+    curso.save()
+    texto= f"curso creado: {curso.nombre} {curso.comision}"
+    return HttpResponse(texto)
+
+# Create your views here.
+
+def entrega(self, nom_ape, documento, ocup):
+    entrega= Familia(nombre_apellido=nom_ape, dni=documento, fecha_actualizacion=1, ocupacion=ocup)
+    entrega.save()
+    texto= f"Se ingreso el registro de {entrega.nombre_apellido} DNI: {entrega.dni} fecha_nacimiento: {entrega.fecha_actualizacion} ocupacion {entrega.ocupacion}."
+    return HttpResponse(texto)
+
+def carga(self):
+    
+    plantilla=loader.get_template('template1.html')
+    documento=plantilla.render()
+    return HttpResponse(documento)

710d6a416159cda14f543e7b916842face40f576
diegoconde86 <diegoconde86@gmail.com>
2022-07-14 00:25:36 -0300

    comit 4

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('curso/    ', curso),
+    path('curso/    ', curso, name='curso'),
     path('carga_datos/', carga),
     path('entrega/<nom_ape>/<documento>/<ocup>',entrega),
-    path('entregables/', entregables),
-    path('estudiantes/', estudiantes),
-    path('profesores/', profesores),
-    path('cursos/', cursos),
-    path('', inicio),
+    path('entregables/', entregables, name='entregables'),
+    path('estudiantes/', estudiantes, name='estudiantes'),
+    path('profesores/', profesores, name= 'profesores'),
+    path('cursos/', cursos, name='cursos'),
+    path('', inicio, name ='inicio'),
 ]

f8b015e534874d43b484bbde9f293f7a67c81dc1
diegoconde86 <diegoconde86@gmail.com>
2022-07-15 11:37:48 -0300

    comit 5

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -7,21 +7,37 @@ class Curso(models.Model):
     nombre=models.CharField(max_length=50)
     comision=models.IntegerField()
 
+    def __str__(self):
+        return self.nombre + " " + str(self.comision)
+
 class Estudiante(models.Model):
     nombre= models.CharField(max_length=50)
     apellido= models.CharField(max_length=50)
     email= models.EmailField()
     
+    def __str__(self):
+        return self.apellido + " " + self.nombre
+
 class Profesor(models.Model):
     nombre= models.CharField(max_length=50)
     apellido= models.CharField(max_length=50)
     email= models.EmailField()
     profesion= models.CharField(max_length=50)
+
+    
+    def __str__(self):
+        return self.apellido + " " + self.nombre
+
+       
     
 class Entregable(models.Model):
     nombre= models.CharField(max_length=50)
     fecha_entrega= models.DateField()
     entregado= models.BooleanField()
+
+    
+    def __str__(self):
+        return self.nombre + " " + str(self.fecha_entrega)
 
 class Familia(models.Model):
     nombre_apellido= models.CharField(max_length=50)
@@ -29,5 +45,6 @@ class Familia(models.Model):
     fecha_actualizacion= models.DateField(auto_now_add=TRUE)
     ocupacion=models.CharField(max_length=50)
     
-
+    def __str__(self):
+        return self.nombre_apellido + " " + str(self.fecha_actualizacion)
 # Create your models here.
